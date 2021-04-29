0017bbe50b69ee441d96b4262ce7598ae86cc2b0
eric88525 <39957434+eric88525@users.noreply.github.com>
2021-04-29 17:22:05 +0800

    Create source.cpp

---
+++ 75. Sort Colors/source.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int left = 0;
+        int right = nums.size()-1;
+        int mid=0;   
+        
+        while( right>=mid ){
+            if (nums[mid] == 2 ){
+                swap( nums[mid] , nums[right--]  );
+            }else if(nums[mid] == 0){
+                swap( nums[mid] , nums[left++] ) ;
+                mid = left ;
+            } else{
+                mid++;
+            }
+        }        
+    }
+};
